61932fb232afd3c79f5cd1c4dd7f5a64fe7d91cb
sieuneee <kseun021@naver.com>
2021-12-05 21:01:06 +0900

    init

---
+++ src/main/java/study/springproject/controller/TestController.java
@@ -0,0 +1,12 @@
+package study.springproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/home")
+    public String getHome(){
+        return "Hello World!";
+    }
+}
